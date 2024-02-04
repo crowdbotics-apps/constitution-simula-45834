40a2734259f23ce132d6b2498deff632cc2162aa
Crowdbotics <team@crowdbotics.com>
2024-02-04 18:00:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "constitution-simula-45834",
+  url: "https://constitution-simula-45834.botics.co",
+  api: "https://constitution-simula-45834.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
